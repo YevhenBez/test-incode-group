c30a8aaabeb944cd3f2269a0fc65f38d861b1771
YevhenBez <byxgalter@bigmir.net>
2024-04-15 15:01:24 +0300

    Набросал примерную логику работы компонента   IssueTracker  и разрендерил его в компоненте   App   Тестовое для компании   Incode Group
    Набросал примерную логику работы компонента   IssueTracker  и разрендерил его в компоненте   App   Для этого в файле  IssueTracker.jsx  импортнул   useState, useEffect   , также импортнул  axios   и компонент   Column   Затем в переменной  IssueTracker   я деструктуризировал   Url   Репы при вводе которой должен отрабатываться весь алгоритм.  Затем в переменной  loadIssues   я написал алгоритм, который загружает проблемы репозитория,   Url   которого получил компонент   IssueTracker   Загруженные проблемы Репо я записываю в   issues   при помощи   setIssues(response.data);   Затем я возвращаю немного разметки, разрендеривая компонент   Column
     При этом в разрендериваемых компонентах   Column  используются фильтры, чтобы полученные  issues  отобразились в соответствующих колонках.   Затем в файле  App.jsx   я импротнул компонент  IssueTracker  и разрендерил его.  Тестовое для компании   Incode Group

--- src/components/issueTracker/IssueTracker.jsx
+++ src/components/issueTracker/IssueTracker.jsx
@@ -1,6 +1,36 @@
-const IssueTracker = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Column from '../column';
+
+
+const IssueTracker = ({ repoUrl }) => {
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    const loadIssues = async () => {
+      try {
+        const apiUrl = `${repoUrl}/issues`;
+        const response = await axios.get(apiUrl);
+        setIssues(response.data);
+      } catch (error) {
+        console.error('Error loading issues:', error);
+      }
+    };
+
+    if (repoUrl) {
+      loadIssues();
+    }
+  }, [repoUrl]);
+
   return (
-    <h1>Это компонент IssueTracker - основной компонент приложения</h1>
+    <div>
+      <h1>GitHub Issue Tracker</h1>
+      <div>
+        <Column title="ToDo" issues={issues.filter(issue => !issue.state || issue.state === 'open')} />
+        <Column title="In Progress" issues={issues.filter(issue => issue.assignee)} />
+        <Column title="Done" issues={issues.filter(issue => issue.state === 'closed')} />
+      </div>
+    </div>
   );
 };
 export default IssueTracker;
